85dbcbe58674ff7eca5de1a280bd858b6519839a
jlev <jlev@tf.czu.cz>
2025-03-11 15:11:15 +0100

    detector.py: add support for segmentation

--- model/detector.py
+++ model/detector.py
@@ -3,6 +3,7 @@
 """
 
 import cv2
+import numpy as np
 import torch
 from ultralytics import YOLO
 
@@ -21,16 +22,23 @@ class Detector:
         """
         Detects objects from image.
         :param image: Input image (BGR).
-        :return: list of detections [(bbox, score)
+        :return: list of detections [(bbox, polygon, score)
         """
         results = self.model(image)
         detections = []
 
         for result in results:
-            for box in result.boxes:
-                x1, y1, x2, y2 = map(int, box.xyxy[0])
-                score = float(box.conf[0].item())
-                detections.append(((x1, y1, x2, y2), score))
+            if result.masks is not None:
+                for box, mask in zip(result.boxes, result.masks.xy):
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    score = float(box.conf.item())
+                    polygon = np.array(mask, np.int32)
+                    detections.append(((x1, y1, x2, y2), polygon, score))
+            else:
+                for box in result.boxes:
+                    x1, y1, x2, y2 = map(int, box.xyxy[0])
+                    score = float(box.conf.item())
+                    detections.append(((x1, y1, x2, y2), None, score))
 
         return detections
 
@@ -40,15 +48,18 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('img', help='Path to image')
-    parser.add_argument('--model', help='Path to model', default="run4_best.pt")
+    parser.add_argument('--model', help='Path to model', default="my_models/run4_best_m.pt")
     args = parser.parse_args()
 
     detector = Detector(model_path=args.model)
     image = cv2.imread(args.img)
     detections = detector.detect(image)
 
-    for (x1, y1, x2, y2), conf in detections:
+    for (x1, y1, x2, y2), polygon, conf in detections:
         cv2.rectangle(image, (x1, y1), (x2, y2), (255, 0, 0), 2)
+        if polygon is not None:
+            # cv2.polylines(image, [polygon], isClosed=False, color=(0, 0, 255), thickness=2)
+            cv2.drawContours(image, [polygon], -1, color=(0, 0, 255), thickness=-1)
         label = f"{conf:.2f}"
         cv2.putText(image, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
 
