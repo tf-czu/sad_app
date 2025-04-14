d6ac734da58032cddc6489c308e3b85bf2c417c2
jlev <jlev@tf.czu.cz>
2025-04-14 17:57:37 +0200

    tree_node.py: refactor and add sync checker

--- tree_node.py
+++ tree_node.py
@@ -1,9 +1,6 @@
 """
     TODO
 """
-from http.cookiejar import debug
-from os import write
-
 import cv2
 import numpy as np
 
@@ -24,24 +21,47 @@ class TreeNode(Node):
         bus.register('tree')  # register a stream to be published
         self.verbose = False
 
+        self.last_color_seq = None
+        self.last_color = None
+
+        self.is_synced_depth_color = None
+
         self.tree_analyse = TreeAnalyse((1080, 1920))
         self.debug_images = []
         self.debug_trees = []
+
+    def on_color_seq(self, data):
+        self.last_color_seq = data
+
+    def on_depth_seq(self, last_depth_seq):
+        # Data are received in order color_seq, color, depth_seq, depth
+        # Check if frames id are the same
+        if last_depth_seq[0] == self.last_color_seq[0]:
+            self.is_synced_depth_color = True
+            # compare internal camera time in us
+            assert abs(last_depth_seq[1] - self.last_color_seq[1]) < 35_000, (self.last_depth_seq, self.last_color_seq)
+        else:
+            self.is_synced_depth_color = False
 
     def on_pose3d(self, data):
         pass
 
     def on_color(self, data):
-        im = cv2.imdecode(np.frombuffer(data, dtype=np.uint8), 1)
+        self.last_color = data
+
+    def on_depth(self, depth_data):
+        # The spring data do not contain seq streams
+       if self.is_synced_depth_color is not False:
+           self.process_data(self.last_color, depth_data)
+
+    def process_data(self, color_data, depth_data):
+        im = cv2.imdecode(np.frombuffer(color_data, dtype=np.uint8), 1)
         assert im.shape == (1080, 1920, 3)
         tree_data, debug_img = self.tree_analyse.process(im)
-        # print(tree_data)
+        self.publish("tree", tree_data)
         if self.verbose:
             self.debug_images.append(debug_img)
             self.debug_trees.append(tree_data)
-
-    def on_depth(self, data):
-        pass
 
 
     def draw(self):
