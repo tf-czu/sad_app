85ee48cc31097c2f1faec601113289f3a45a4965
jlev <jlev@tf.czu.cz>
2025-07-30 14:19:15 +0200

    add supsample

--- tools/log2images.py
+++ tools/log2images.py
@@ -4,7 +4,7 @@ from osgar.logger import LogReader, lookup_stream_id
 from osgar.lib.serialize import deserialize
 
 
-def main(log_file, im_stream, out_dir):
+def main(log_file, im_stream, out_dir, subsample):
     only_im = lookup_stream_id(log_file, im_stream)
 
     with LogReader(log_file, only_stream_id=only_im) as log:
@@ -12,7 +12,7 @@ def main(log_file, im_stream, out_dir):
         for timestamp, stream_id, data in log:
             if stream_id == only_im:
                 jpeg_im = deserialize(data)
-                if ii%50 == 0:
+                if ii%subsample == 0:
                     with open(os.path.join(out_dir, f"im_{ii:05d}.jpg"), "wb") as f:
                         f.write(jpeg_im)
                 ii += 1
@@ -24,6 +24,7 @@ if __name__ == "__main__":
     parser.add_argument('logfile', help='path to logfile')
     parser.add_argument('--im', help='image stream', default="oak_camera.color")
     parser.add_argument('--out', help='out directory', default="logs/tmp_im")
+    parser.add_argument('--subsample', help='subsample', default=1)
     args = parser.parse_args()
 
-    main(args.logfile, args.im, args.out)
+    main(args.logfile, args.im, args.out, args.subsample)
