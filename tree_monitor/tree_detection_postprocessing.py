dca996c8444c6fe1dd24a43655a571880095cc13
jlev <jlev@tf.czu.cz>
2025-12-17 18:16:15 +0100

    add export of detections

--- tree_monitor/tree_detection_postprocessing.py
+++ tree_monitor/tree_detection_postprocessing.py
@@ -3,6 +3,7 @@
 """
 import os
 import cv2
+import json
 
 from tree_analyse import TreeAnalyse
 
@@ -13,12 +14,14 @@ class TreeDetection:
         self.tree_analyse = TreeAnalyse((1080, 1920), model_path)
         self.result_dir = os.path.join(self.images_path, "results")
         os.makedirs(self.result_dir)
+        self.annotations = {}
 
     def process_data(self, im_name):
         im = cv2.imread(os.path.join(self.images_path, im_name))
         assert im.shape == (1080, 1920, 3)
         tree_data, debug_img = self.tree_analyse.process(im)
         self.draw(debug_img, im_name)
+        return tree_data
 
 
     def draw(self, img, im_name):
@@ -27,7 +30,16 @@ class TreeDetection:
     def run_detection(self):
         for im_name in sorted(os.listdir(self.images_path)):
             if im_name.lower().endswith((".jpg", ".jpeg")):
-                self.process_data(im_name)
+                tree_data = self.process_data(im_name)
+                if tree_data:
+                    canopy_list = []
+                    for __, canopy in tree_data:
+                        canopy_list.append([ar.tolist() for ar in canopy])  # arr to list due json
+
+                    self.annotations[im_name] = canopy_list
+
+        with open(os.path.join(self.result_dir, 'annotation.json'), 'w', encoding='utf-8') as f:
+            json.dump(self.annotations, f, ensure_ascii=False, indent=4)
 
 
 if __name__ == "__main__":
