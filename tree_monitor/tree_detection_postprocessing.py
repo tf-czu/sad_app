a5f5ea67b44caee475533e99a7546f4cc88498e8
jlev <jlev@tf.czu.cz>
2026-06-30 18:22:06 +0200

    tree_detection_postprocessing.py: update plot output combine both models

--- tree_monitor/tree_detection_postprocessing.py
+++ tree_monitor/tree_detection_postprocessing.py
@@ -9,6 +9,8 @@ import matplotlib.pyplot as plt
 
 from ast import literal_eval
 
+from sympy.physics.optics import medium
+
 from tree_analyse import TreeAnalyse
 
 
@@ -49,36 +51,37 @@ class TreeDetection:
             json.dump(self.annotations, f, ensure_ascii=False, indent=4)
 
         if self.verbose:
-            self.process_debug_data()
+            return self.tree_analyse.debug_area_ratio, self.tree_analyse.debug_spacing
 
-    def process_debug_data(self):
-        print("debug_area_ratio")
-        print(self.tree_analyse.debug_area_ratio)
-        print("debug_spacing")
-        print(self.tree_analyse.debug_spacing)
+def process_debug_data(debug_area_ratio, debug_spacing):
+    print("debug_area_ratio")
+    print(debug_area_ratio)
+    print("debug_spacing")
+    print(debug_spacing)
 
-        fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(8, 4))
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(6, 3))
 
-        ax1.hist(self.tree_analyse.debug_area_ratio, bins=30, color='dimgrey', edgecolor='black', alpha=0.7)
-        ax1.set_xlabel('Area ratio')
-        ax1.set_ylabel('Frequency')
+    ax1.hist(debug_area_ratio, bins=30, color='dimgrey', edgecolor='black', alpha=0.7)
+    ax1.set_xlabel('Area ratio (-)', fontsize=12)
+    ax1.set_ylabel('Frequency (-)', fontsize=12)
 
-        ax2.hist(self.tree_analyse.debug_spacing, bins=30, color='dimgrey', edgecolor='black', alpha=0.7)
-        ax2.set_xlabel('Bounding box distances')
-        ax2.set_ylabel('Frequency')
+    distances = [d*0.97 for d in debug_spacing]
+    ax2.hist(distances, bins=30, color='dimgrey', edgecolor='black', alpha=0.7)
+    ax2.set_xlabel('Detection distance (mm)', fontsize=12)
+    ax2.set_ylabel('Frequency (-)', fontsize=12)
 
-        # Auto label format (avoid overlap)
-        plt.tight_layout()
+    # Auto label format (avoid overlap)
+    plt.tight_layout()
 
-        # plt.savefig('histograms.png', dpi=300)
-        plt.show()
+    plt.savefig('histograms.png', dpi=1200)
+    # plt.show()
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('images', help='path to image dir')
-    parser.add_argument('--models', help='Path to models', default="tree_monitor/model/my_models/medium/")
+    parser.add_argument('--models', help='Path to models, or string "both" (use both models)', default="tree_monitor/model/my_models/medium/")
     parser.add_argument('--out_dir', help='Output directory for results', default="results")
     parser.add_argument('--margin', help='Ignored edge distance, float or list', default="0.01")
     parser.add_argument('--spacing', help='Spacing between trees (px), int or list of ints', default="300")
@@ -106,5 +109,21 @@ if __name__ == "__main__":
                 detect = TreeDetection(args.images, args.models, out_dir, spacing = spa, min_area = area)
                 detect.run_detection()
     else:
-        detect = TreeDetection(args.images, args.models, args.out_dir, verbose = True)
-        detect.run_detection()
+        if args.models == "both":
+            models = [
+                "tree_monitor/model/my_models/nano/",
+                "tree_monitor/model/my_models/medium/"
+            ]
+            ratios = []
+            spacings = []
+            for model, out_dir in zip(models, ["margin_res_nano", "margin_res_medium"]):
+                print(f"Process data with model: {model}")
+                detect = TreeDetection(args.images, model, out_dir, verbose = True)
+                debug_area_ratio, debug_spacing = detect.run_detection()
+                ratios.extend(debug_area_ratio)
+                spacings.extend(debug_spacing)
+            process_debug_data(ratios, spacings)
+
+        else:
+            detect = TreeDetection(args.images, args.models, args.out_dir)
+            detect.run_detection()
