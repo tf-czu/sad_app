1585efd348d51da2bd47fbfb6be5c901288e15ef
jlev <jlev@tf.czu.cz>
2026-09-09 14:55:46 +0200

    fix_coco.py: remove unuse class

---
+++ tree_monitor/model/fix_coco.py
@@ -0,0 +1,15 @@
+import json
+
+def fix_coco_json(json_path, output_path):
+    with open(json_path, 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    data['categories'] = [
+        {"id": 1, "name": "canopy", "supercategory": "none"}
+    ]
+
+    with open(output_path, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=2)
+
+fix_coco_json("train/_annotations.coco.json", "train/_annotations_fixed.json")
+fix_coco_json("valid/_annotations.coco.json", "valid/_annotations_fixed.json")
