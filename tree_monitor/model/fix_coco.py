d4e6b32f971e6720b45539f22a0b0e040e1a0080
jlev <jlev@tf.czu.cz>
2026-09-10 11:56:13 +0200

    fix_coco.py: fix path to coco

--- tree_monitor/model/fix_coco.py
+++ tree_monitor/model/fix_coco.py
@@ -11,6 +11,6 @@ def fix_coco_json(json_path, output_path):
     with open(output_path, 'w', encoding='utf-8') as f:
         json.dump(data, f, indent=2)
 
-fix_coco_json("train/_annotations.coco.json", "train/_annotations_fixed.json")
-fix_coco_json("valid/_annotations.coco.json", "valid/_annotations_fixed.json")
-fix_coco_json("test/_annotations.coco.json", "test/_annotations_fixed.json")
+fix_coco_json("coco-seg-dataset/train/_annotations.coco.json", "coco-seg-dataset/train/_annotations_fixed.json")
+fix_coco_json("coco-seg-dataset/valid/_annotations.coco.json", "coco-seg-dataset/valid/_annotations_fixed.json")
+fix_coco_json("coco-seg-dataset/test/_annotations.coco.json", "coco-seg-dataset/test/_annotations_fixed.json")
